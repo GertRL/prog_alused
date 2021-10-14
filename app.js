10f67fd76c77d71a18c6274d875e9f61c6c142c7
Gert-Ranno Lumine <gert-ranno.kask@khk.ee>
2021-10-14 09:01:57 +0300

    tekstiväljastamine konsooli ja kommenteerimine

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("hello world")
+console.log("see"+" on" + " liitlause")
+
+//  console.log("see"+" on" + " kommentaar")  üherealine kommentaar
+
+/* Mitmerealine kommentaar
+
+console.log("hello world")
+console.log("see"+" on" + " liitlause")
+
+ */
