8c8dbafc42f1284fab7da4f37cd5ceed244677cc
Gert-Ranno Lumine <gert-ranno.kask@khk.ee>
2021-10-28 10:49:09 +0300

    äratuse ülesanne

---
+++ äratus.js
@@ -0,0 +1,5 @@
+let kordus = prompt("Mitu korda sa soovid äratust korrata?")  // küsib mitu korda äratada
+
+for (let kord = 1 ; kord <= kordus; kord++) {                       // kordab mitu korda sooviti
+    console.log(kord +". kord tõuse ja sära")
+}
